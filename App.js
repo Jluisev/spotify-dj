9d3c74cb474711240057ce66b81a27203cfa3b79
Jose Esquivel <jose.esquivel@unosquare.com>
2018-02-24 16:37:13 -0600

    detach exp

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import Navigator from './src/navigator'
 import store from './src/store'
+import Expo from 'expo'
 
 export default class App extends React.Component {
   render() {
+    console.log(Expo.Constants.linkingUri)
     return (
       <Provider store={store}>
         <Navigator />
---
+++ ios/Pods/Target Support Files/Pods-spotify-dj/Pods-spotify-dj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_spotify_dj : NSObject
+@end
+@implementation PodsDummy_Pods_spotify_dj
+@end
